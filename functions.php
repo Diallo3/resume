fcc9e0713149d7ce8f6e813388838fbd67ec8d4e
Diallo Hudgins <d@diallohudgins.com>
2017-01-30 23:03:22 -0500

    added timber menus to construct

--- functions.php
+++ functions.php
@@ -11,6 +11,9 @@
 // TGM Plugin
 require_once(get_template_directory().'/lib/tgm/tgm-activate.php');
 
+// Timber Spport
+require_once(get_template_directory(). '/assets/functions/timber-const.php');
+
 // Theme support options
 require_once(get_template_directory().'/assets/functions/theme-support.php'); 
 
@@ -53,44 +56,4 @@ require_once(get_template_directory().'/assets/translation/translation.php');
 // Customize the WordPress admin
 // require_once(get_template_directory().'/assets/functions/admin.php'); 
 
-// Timber construct
-if ( ! class_exists( 'Timber' ) ) {
-	add_action( 'admin_notices', function() {
-			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
-		} );
-	return;
-}
 
-class OpusWp extends TimberSite {
-	
-	function __construct() {          
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-	}
-
-	function add_to_context($context){
-	    /* this is where you can add your own data to Timber's context object */
-	    $context['foo'] = 'bar';
-	    $context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['header'] = new TimberMenu('header-nav');
-		$context['footer'] = new TimberMenu('footer-nav');
-		$context['site'] = $this;
-	    return $context;
-	}
-
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own fuctions to twig */
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
-		return $twig;
-	}
-
-	function myfoo( $text ) {
-		$text .= ' bar!';
-		return $text;
-	}
-
-
-}
-new OpusWp();
